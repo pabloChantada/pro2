09539b61ae1e82500dd84fb3d25dd76e0d04279d
ChantaClaun <pablo.chantada@udc.es>
2024-02-28 14:15:30 +0100

    terminar to-do

--- practica1/estadisticas.py
+++ practica1/estadisticas.py
@@ -1,41 +1,94 @@
 import pandas
 import numpy
-'''
-cambiarlo a en vez de numeros a listas, por eso no funciona
-3 datagramas por pantalla
-'''
-data = {
-    "Fire": {"Fire": 0, "Water": 0, "Grass": 0, "Damage": 0, "Healing": 0, "Counter Type": 0},
-    "Water": {"Fire": 0, "Water": 0, "Grass": 0, "Damage": 0, "Healing": 0, "Counter Type": 0},
-    "Grass": {"Fire": 0, "Water": 0, "Grass": 0, "Damage": 0, "Healing": 0, "Counter Type": 0}
+
+type_type_data = {
+    "Fire": {"Fire": [], "Water": [], "Grass": []},
+    "Water": {"Fire": [], "Water": [], "Grass": []},
+    "Grass": {"Fire": [], "Water": [], "Grass": []}
 }
 
-indv_data = {
-    "Name": "",
-    "Damage": []
+type_data = {
+    "Fire": {"Damage": [], "Healing": []},
+    "Water": {"Damage": [], "Healing": []},
+    "Grass": {"Damage": [], "Healing": []}
 }
+indv_data = {}
 
-type_data = {}  
-type_type_data = {}
+
+def stats_indv(attacker, damage, healing=0):
+    if attacker.name not in indv_data:
+        indv_data.setdefault(
+            attacker.name, {"Damage": [damage], "Healing": [healing]})
+    else:
+        indv_data[attacker.name]["Damage"].append(damage)
+        indv_data[attacker.name]["Healing"].append(healing)
+
+
+def stats_type(attacker, damage, healing=0):
+    match attacker.pokemon_type:
+        case "Fire":
+            type_data["Fire"]["Damage"].append(damage)
+            type_data["Fire"]["Healing"].append(healing)
+        case "Grass":
+            type_data["Grass"]["Damage"].append(damage)
+            type_data["Grass"]["Healing"].append(healing)
+        case "Water":
+            type_data["Water"]["Damage"].append(damage)
+            type_data["Water"]["Healing"].append(healing)
+
+
+def stats_type_type(attacker, defender, damage):
+    type_type_data[attacker.pokemon_type][defender.pokemon_type].append(damage)
+
 
 def update_damage(attacker, defender, damage, healing=0):
     '''
     Crear un dict por pokemon y como value tiene el daño por ronda
     '''
-    if attacker.name in indv_data:
-        indv_data["Damage"].append(damage)
-    else:
-        indv_data.update({"attacker.name", "damage"})
-    #type_data[attacker.pokemon_type] = {"Damage": damage, "Healing": healing, "Counter Indv": + 1}
-    #type_type_data[attacker.pokemon_type][defender.pokemon_type] = {"Fire": 0, "Water": 0, "Grass": 0, "Damage": 0, "Healing": 0, "Counter Type": 0}
+    stats_indv(attacker, damage, healing)
+    stats_type(attacker, damage, healing)
+    stats_type_type(attacker, defender, damage)
 
-def mean_indv(data):
-    pass
+
+def mean(data):
+    return sum(data) / len(data) if len(data) > 0 else 0
+
+
+def std(data):
+    return numpy.std(data) if len(data) > 0 else 0
+
 
 def stats():
+    indexes = ["Avg Damage", "Avg Healing", "Std Damage", "Std Healing"]
+    index_type_type = ["Fire vs Fire", "Fire vs Water", "Fire vs Grass",
+                         "Water vs Fire", "Water vs Water", "Water vs Grass",
+                         "Grass vs Fire", "Grass vs Water", "Grass vs Grass"]
+    col_type_type = ["Avg Damage", "Std Damage"]
+    df_indv = pandas.DataFrame(indv_data, indexes)
+    df_type = pandas.DataFrame(type_data, indexes)
+    df_type_type = pandas.DataFrame(type_type_data, index_type_type, col_type_type)
+
+    for key, value in indv_data.items():
+        df_indv[key] = [round(mean(value['Damage']), 2), round(mean(value['Healing']), 2), round(
+            std(value['Damage']), 2), round(std(value['Healing']), 2)]
+
+    for key, value in type_data.items():
+        df_type[key] = [round(mean(value['Damage']), 2), round(mean(value['Healing']), 2), round(
+            std(value['Damage']), 2), round(std(value['Healing']), 2)]
+    for key, value in type_type_data.items():
+        print(key,"--->", value)
+        print(key,"--->", mean(value['Fire']))
+        print(key,"--->", mean(value['Water']))
+        print(key,"--->", mean(value['Grass']))        
+        '''df_type_type[key] = \
+            [round(mean(value['Fire']), 2), \
+            round(mean(value['Water']), 2),\
+            round(mean(value['Grass']), 2), \
+            round(std(value['Fire']), 2), \
+            round(std(value['Water']), 2), \
+            round(std(value['Grass']), 2)]'''
+
     
-    df_indv = pandas.DataFrame()
-    df_type = pandas.DataFrame(type_data)
-    df_type_type = pandas.DataFrame(type_type_data)
-    for i in range(len(indv_data)):
-        print(indv_data)    
+    print(df_indv)
+    print(df_type)
+    print(df_type_type)
--- practica1/todo.md
+++ practica1/todo.md
@@ -1,22 +1,7 @@
-Pelea entre 2 entrenadores y sus Pokemon
-- Se pelea hasta que todos los pokemons son derrotados
-# Fase 1
-[-] Jerarquia de clases:
-    - Clase abstracta Pokemon
-        - Basic_attack
-        - is_debilitated
-        - effectiveness -> implementaod en las hijas
-    - Clases hijas:
-        - Agua
-        - Fuego
-        - Planta
-    - Creado en __pokemon.py__
-    - El atributo tipo de pokemon se define en el constructor de la subclase
-    - Implementar getter y setters para dichos atributos -> __property_example.py__
-    
-# Fase 2
-Mirar mejor la logica
-# Fase 3
-
-mirar la curacion si esta fbien
-alguno tipos no se atacan ??
+[-] Documentar
+[-] Manual de Usuario
+[-] Arreglar estadisticas
+[-] Mirar de mejorar la logica
+[-] Arreglar daños = 0
+[-] Arreglar Strings
+[-] Cambiar str de temperature y asi a float en parse de main
