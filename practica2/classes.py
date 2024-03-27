13972a5618a55e421ec3087dc89675f4e1f3bef9
ChantaClown <pablo.chantada@udc.es>
2024-03-27 18:59:03 +0100

    Terminado

--- practica2/classes.py
+++ practica2/classes.py
@@ -1,130 +1,295 @@
-from materiales.array_queue import ArrayQueue
+'''
+Pablo Chantada Saborido | pablo.chantada@udc.es
+Pablo Verdes Sánchez | p.verdess@udc.es
+'''
+
+from array_queue import ArrayQueue
 
 class Process():
     def __init__(self, id_process: str, id_user: str, resource_type: str, time_to_process: str, exec_time: int):
+        '''
+        Crea una instancia de la clase Process.
+
+        Arguments
+        ----------
+            id_process (str): El ID del proceso.
+            id_user (str): El ID del usuario asociado al proceso.
+            resource_type (str): El tipo de recurso asociado al proceso.
+            time_to_process (str): El tiempo necesario para procesar el recurso.
+            exec_time (int): El tiempo de ejecución del proceso.
+
+        Attributes
+        ----------
+            _id_process (str): El ID del proceso.
+            _id_user (str): El ID del usuario asociado al proceso.
+            resource_type (str): El tipo de recurso asociado al proceso.
+            time_to_process (str): El tiempo necesario para procesar el recurso.
+            exec_time (int): El tiempo de ejecución del proceso.
+            join_time (int): El tiempo de unión del proceso.
+            start_execution (int): El tiempo de inicio de ejecución del proceso.
+        '''
         self._id_process = id_process
         self._id_user = id_user
-        self._resource_type = resource_type
-        self._time_to_process = time_to_process
-        self._exec_time = exec_time
-        self._join_time = 0
-        self._start_execution = 0
-        
+        self.resource_type = resource_type
+        self.time_to_process = time_to_process
+        self.exec_time = exec_time
+        self.join_time = 0
+        self.start_execution = 0
+
     @property
     def id_process(self):
         return self._id_process
+
     @id_process.setter
     def id_process(self, value):
         if isinstance(value, str) and len(value) > 0:
-            self._name = value
+            self._id_process = value
         else:
             raise ValueError("ID must be a non-empty string")
-        
+
     @property
     def id_user(self):
         return self._id_user
+
     @id_user.setter
     def id_user(self, value):
         if isinstance(value, str) and len(value) > 0:
             self._id_user = value
         else:
             raise ValueError("ID must be a non-empty string")
-        
+
     @property
     def resource_type(self):
         return self._resource_type
+
     @resource_type.setter
     def resource_type(self, value):
         if isinstance(value, str) and len(value) > 0:
             self._resource_type = value
         else:
             raise ValueError("Resource Type must be a non-empty string")
-    
+
     @property
-    def exc_time(self):
-        return self._exc_time
-    @exc_time.setter
-    def exc_time(self, value):
-        if isinstance(value, str) and len(value) > 0:
-            self._exc_time = value
+    def exec_time(self):
+        return self._exec_time
+
+    @exec_time.setter
+    def exec_time(self, value):
+        if isinstance(value, int) and value >= 0:
+            self._exec_time = value
         else:
             raise ValueError("Execution time must be a non-negative integer")
     
     @property
-    def starting_time(self):
-        return self._starting_time
-    @starting_time.setter
-    def starting_time(self, value):
+    def join_time(self):
+        return self._join_time
+    @join_time.setter
+    def join_time(self, value):
         if isinstance(value, int) and value >= 0:
-            self._starting_time = value
+            self._join_time = value
         else:
-            raise ValueError("Starting time must be a non-negative integer")
-    
+            raise ValueError("Join time must be a non-negative integer")
+        
+    @property
+    def start_execution(self):
+        return self._start_execution
+    @start_execution.setter
+    def start_execution(self, value):
+        if isinstance(value, int) and value >= 0:
+            self._start_execution = value
+        else:
+            raise ValueError("Start execution time must be a non-negative integer")
+        
     def print(self):
-        if self is not None:
-            print(f"ID: {self.id_process}, User: {self.id_user}, Resource: {self.resource_type}, Exc Time: {self.exc_time}, Starting Time: {self.starting_time}")
+        '''
+        Imprime los detalles del proceso.
+        '''
+        print(f"ID: {self._id_process}, User: {self._id_user}, Resource: {self.resource_type}, Exc Time: {self.exec_time}, Starting Time: {self.start_execution}")
 
-    
+
 class ManagerQueue():
-    def __init__(self):
-        self.cpu_short = ArrayQueue()
-        self.cpu_long = ArrayQueue()
-        self.gpu_short = ArrayQueue()
-        self.gpu_long = ArrayQueue()
-        self.penalties = []
-        
-    def all_empty(self):
-        return self.cpu_short.is_empty() and self.cpu_long.is_empty() and self.gpu_short.is_empty() and self.gpu_long.is_empty()
-    
-    def manage_queue(self, process: Process):
-        match (process.resource_type, process.exc_time):
-            case ("cpu", "short"):
-                self.cpu_short.enqueue(process)
-            case ("cpu", "long"):
-                self.cpu_long.enqueue(process)
-            case ("gpu", "short"):
-                self.gpu_short.enqueue(process)
-            case ("gpu", "long"):
-                self.gpu_long.enqueue(process)
-            case (_, _):
-                raise Exception("Resource type not found")    
-    
-    
-    # Seguir desde el c del 3
-    def execute_process(self, process_queue: ArrayQueue, time: int):
-        if self.cpu_short.first() not in process_queue:
-            if self.cpu_short.first().id_user in self.penalties:
-                process = self.cpu_short.dequeue()
-                self.cpu_long.enqueue(process)
-                self.penalties.pop(self.penalties.index(process.id_user))
-                
-            pc_short = self.cpu_short.enqueue(process_queue.dequeue())
-            pc_short._start_execution = time
-            
-        if self.cpu_long.first() not in process_queue:
-            pc_long = self.cpu_long.enqueue(process_queue.dequeue())
-            pc_long._start_execution = time
-            
-        if self.gpu_short.first() not in process_queue:
-            if self.gpu_short.first().id_user in self.penalties:
-                process = self.gpu_short.dequeue()
-                self.gpu_long.enqueue(process)
-                self.penalties.pop(self.penalties.index(process.id_user))
-            pg_short = self.gpu_short.enqueue(process_queue.dequeue())
-            pg_short._start_execution = time
-            
-        if self.gpu_long.first() not in process_queue:
-            pg_long = self.gpu_long.enqueue(process_queue.dequeue())
-            pg_long._start_execution = time
-        
+    '''
+    Clase que representa un administrador de colas de procesos.
 
+    Attributes
+    ----------
+    - queues(dict): Diccionario que almacena las colas de procesos por tipo de recurso y duración.
+    - penalties(list): Lista que almacena los usuarios penalizados.
+    - active_process(list): Lista que almacena los procesos activos.
+    - stats_penalties(list): Lista que almacena las estadísticas de penalizaciones por usuario.
+    - stats_process(list): Lista que almacena las estadísticas de procesos ejecutados.
+
+    Methods
+    -------
+    - all_empty(): Verifica si todas las colas están vacías.
+    - all_processes_finished(): Verifica si todos los procesos han finalizado.
+    - add_penalty(process, time): Agrega una penalización a un usuario.
+    - execute_panalty(process, time): Ejecuta una penalización y mueve el proceso a la cola de larga duración.
+    - manage_queue(process, time): Administra la cola de ejecución de procesos.
+    - check_process(time): Verifica si los procesos activos han terminado.
+    - process_queues(time): Procesa las colas de procesos y ejecuta los procesos correspondientes.
+    - is_active_process_of_type(queue_type): Verifica si hay un proceso activo del mismo tipo.
+    '''
+    def __init__(self):
+        self.queues = {"cpu_short": ArrayQueue(),
+                       "cpu_long": ArrayQueue(),
+                       "gpu_short": ArrayQueue(),
+                       "gpu_long": ArrayQueue()}
+        self.penalties = []
+        self.active_process = []
+        self.stats_penalties = []
+        self.stats_process = []
+            
+    def all_empty(self):
+        '''
+        Verifica si todas las colas están vacías.
+
+        Returns
+        -------
+        - True si todas las colas están vacías.
+        - False si al menos una cola no está vacía.
+        '''
+        # Recorremos todas las colas y verificamos si están vacías
+        return all(queue.is_empty() for queue in self.queues.values())
+
+    def all_processes_finished(self):
+        '''
+        Verifica si todos los procesos han finalizado.
+
+        Returns
+        -------
+        - True si no hay procesos activos.
+        - False si hay al menos un proceso activo.
+        '''
+        # Si es None, devuelve True, si no, False
+        return not self.active_process
+
+    def add_penalty(self, process: Process, time: int):
+        '''
+        Agrega una penalización a un usuario.
+
+        Returns
+        -------
+        - process (Process): proceso penalizado.
+        - time (int): tiempo en el que se aplica la penalización.
+        '''
+        # Lo agregamos a la lista de penalizados
+        self.penalties.append(process._id_user)
+        print(f"Penalización activa: <{time}><{process._id_user}>")
+
+    def execute_panalty(self, process: Process, time: int):
+        '''
+        Ejecuta una penalización y mueve el proceso a la cola de larga duración correspondiente.
+
+        Parameters
+        ----------
+        - process (Process): proceso penalizado.
+        - time (int): tiempo en el que se ejecuta la penalización.
+        '''
+        # Obtenemos el tipo de cola a la que pertenece el proceso
+        type = f"{process.resource_type}_{process.time_to_process}"
+        # Extraemos el proceso de la cola y lo movemos a la cola de larga duración
+        tmp = self.queues[type].dequeue()
+        self.queues[f"{process.resource_type}_long"].enqueue(tmp)
+        # Eliminamos la penalización
+        self.penalties.remove(process._id_user)  # Desactivar penalización
+        print(f"Penalización aplicada: <{time}><{process._id_process}><{process._id_user}>")
         
-        
-        
-    
-    def check_process(self, process: Process, time: int):
-        pass
-    
-    def add_panalty(self, process: Process):
-        # mejor con penalty (id_user, 1)
-        self.penalties.append(process.id_user)
+        # Actualizar estadísticas de penalizaciones
+        if process._id_user not in [x[0] for x in self.stats_penalties]:
+            self.stats_penalties.append([process._id_user, 1])  # Si no se encuentra en la lista
+        else:
+            for penalty in self.stats_penalties:
+                if penalty[0] == process._id_user:
+                    penalty[1] += 1  # Incrementar el conteo de penalizaciones
+                    break
+            
+    def manage_queue(self, process: Process, time: int):
+        '''
+        Administra la cola de ejecución de procesos.
+
+        Parameters
+        ----------
+        - process (Process): proceso a agregar a la cola.
+        - time (int): momento en el que se añade el proceso a una cola.
+        '''
+        # Obtenemos a que cola pertenece el proceso y lo añadimos a la cola correspondiente
+        queue_key = f"{process.resource_type}_{process.time_to_process}"
+        self.queues[queue_key].enqueue(process)
+        # Actualizamos el tiempo de unión del proceso
+        process._join_time = time
+        print(f"Proceso añadido a cola de ejecución: <{time}><{process._id_process}><{process._id_user}><{process.resource_type}><{process.exec_time}>")
+
+    def check_process(self, time: int):
+        '''
+        Verifica si los procesos activos han terminado.
+
+        Parameters
+        ----------
+        - time (int): tiempo actual.
+        '''
+        # Almacenamos los procesos a eliminar
+        remove_process = []
+        # Recorremos los procesos activos
+        for acutual_process in self.active_process:
+            process_time = acutual_process.start_execution + acutual_process.exec_time
+            if time >= process_time:
+                print(f"Proceso terminado: <{time}><{acutual_process._id_process}><{acutual_process._id_user}>")
+                # Eliminamos el proceso de la lista de activos
+                remove_process.append(acutual_process)
+                # Almacenar estadísticas de procesos
+                queue_type = f"{acutual_process.resource_type}_{acutual_process.time_to_process}"
+                self.stats_process.append([queue_type, time - acutual_process._join_time])
+                # Comprobamos si el proceso debe ser penalizado
+                if acutual_process.time_to_process == "short" and process_time > 5:
+                    self.add_penalty(acutual_process, time)
+        # Eliminamos los procesos terminados
+        for process in remove_process:
+            self.active_process.remove(process)
+            
+    def process_queues(self, time):
+        '''
+        Procesa las colas de procesos y ejecuta los procesos correspondientes.
+
+        Parameters
+        ----------
+        - time(int): tiempo actual.
+        '''
+        # Recorremos las colas
+        for queue_type, queue in self.queues.items():
+            # Verificar si la cola no está vacía y no hay un proceso activo del mismo tipo
+            if not queue.is_empty() and not self.is_active_process_of_type(queue_type):
+                process = queue.first()  # Mirar el primer elemento sin desencolarlo
+                # Si el proceso pertenece a un usuario penalizado y es de tipo short
+                if process.id_user in self.penalties and 'short' in queue_type:
+                    # Ejecutamos la penalizacion
+                    self.execute_panalty(process, time)
+                else:
+                    # Iniciar ejecución del proceso
+                    process = queue.dequeue()
+                    # Actualizar tiempo de inicio de ejecución
+                    process._start_execution = time
+                    # Agregar a los procesos activos
+                    self.active_process.append(process)
+
+    def is_active_process_of_type(self, queue_type):
+        '''
+        Verifica si hay un proceso activo del mismo tipo (cpu/gpu y short/long).
+
+        Parameters
+        ----------
+        - queue_type(str): tipo de cola a verificar.
+
+        Returns
+        -------- 
+        - True si hay un proceso activo del mismo tipo.
+        - False si no hay un proceso activo del mismo tipo.
+        '''
+        # Extraemos el tipo de recurso y duración
+        resource_type, duration_type = queue_type.split('_')
+        # Recorremos los procesos activos
+        for process in self.active_process:
+            # Si hay un proceso del mismo tipo en la lista de activos retornamos True
+            if resource_type in process.resource_type and duration_type in process.time_to_process:
+                return True
+        return False
--- practica2/main.py
+++ practica2/main.py
@@ -1,61 +1,165 @@
+'''
+Pablo Chantada Saborido | pablo.chantada@udc.es
+Pablo Verdes Sánchez | p.verdess@udc.es
+'''
+
 import sys
+import pandas as pd
+import matplotlib.pyplot as plt
 from classes import Process, ManagerQueue
-from materiales.array_queue import ArrayQueue
+from array_queue import ArrayQueue
+
+class Statistics():
+    '''
+    Clase para realizar cálculos estadísticos y visualizar los resultados.
+
+    Methods
+    -------
+    - penalization_mean: Calcula el número de penalizaciones por usuario.
+    - show_penalization_mean: Muestra un gráfico de barras con las penalizaciones agrupadas por usuario.
+    - stay_mean: Calcula el tiempo de espera promedio por tipo de cola.
+    - show_stay_mean: Muestra un gráfico de barras con los tiempos de espera promedio por tipo de cola.
+    '''
+    
+    def penalization_mean(self, data):
+        '''
+        Calcula el número de penalizaciones por usuario.
+
+        Parameters
+        ----------
+        - data: Lista de listas con los datos de penalización por usuario.
+
+        Returns
+        -------
+        - penalization_count: DataFrame con el número de penalizaciones por usuario.
+        '''
+        # Generamos el dataframe con los datos de penalización por usuario
+        df = pd.DataFrame(data, columns=["User", "Penalization"])
+        # Agrupamos por usuario y mostramos la media y la suma de las penalizaciones
+        result = df.groupby("User").agg({"Penalization" :["mean","sum"]}).round(2)
+        return result 
+    
+    def show_penalization_mean(self, data):    
+        '''
+        Muestra un gráfico de barras con las penalizaciones agrupadas por usuario.
+        
+        Parameters
+        ----------
+        - data: DataFrame con los datos de penalización por usuario.
+        '''
+        # Reiniciamos el índice del dataframe, se convierten el indice en una columna
+        data_reset = data.reset_index()
+        # Obtenemos los usuarios y las penalizaciones
+        users = data_reset[data_reset.columns[0]]
+        penalizations = data_reset[data_reset.columns[1]]
+        
+        # Crear el gráfico de barras
+        plt.bar(users, penalizations)
+        plt.xlabel('Usuario')                               # Etiqueta eje x
+        plt.ylabel('Penalización')                          # Etiqueta eje y
+        plt.title('Penalizaciones agrupadas por Usuario')   # Título del gráfico
+        plt.show()                                          # Mostrar gráfico
+    
+        
+    def stay_mean(self, data):
+        '''
+        Calcula el tiempo de espera promedio por tipo de cola.
+
+        Parameters
+        ----------
+        - data: Lista de listas con los datos de tiempo de espera por tipo de cola.
+
+        Returns
+        -------
+        - mean_wait_times: DataFrame con el tiempo de espera promedio por tipo de cola.
+        '''
+        # Generamos el dataframe con los datos de tiempo de espera por tipo de cola
+        df = pd.DataFrame(data, columns=["Tipo de Cola", "Tiempo de Espera"])
+        # Agrupamos por tipo de cola y mostramos la media y la suma de los tiempos de espera
+        result = df.groupby("Tipo de Cola").agg({"Tiempo de Espera" :["mean","sum"]}).round(2)
+        return result
+    
+    def show_stay_mean(self, data): 
+        '''
+        Muestra un gráfico de barras con los tiempos de espera promedio por tipo de cola.
+
+        Parameters
+        ----------
+        - data: DataFrame con los datos de tiempo de espera promedio por tipo de cola.
+        '''
+        # Reiniciamos el índice del dataframe, se convierten el indice en una columna
+        data_reset = data.reset_index()
+        # Obtenemos los tipos de cola y los tiempos de espera
+        queue_type = data_reset[data_reset.columns[0]] 
+        time = data_reset[data_reset.columns[1]]
+        
+        # Crear el gráfico de barras
+        plt.bar(queue_type, time)
+        plt.xlabel('Tipo de Cola')                                       # Etiqueta eje x
+        plt.ylabel('Tiempo de Espera Promedio')                          # Etiqueta eje y                
+        plt.title('Tiempos de Espera Promedio por Tipo de Cola')         # Título del gráfico
+        plt.show()                                                       # Mostrar gráfico                 
 
 def parse_file(lines):
-    # Con el espacio no va muy bien, pero con el punto y coma si
-    lines = [x.replace(" ",";") for x in lines]
-    lines = [x.strip("") for x in lines]
-    for i in range(len(lines)):
-        lines[i] = lines[i].split(";")
+    '''
+    Convierte las líneas de un archivo en una cola de registros.
 
+    Parameters
+    ----------
+    lines (list): documento csv con los datos a procesar.
+
+    Returns
+    -------
+    record_queue(queue): Una cola de registros.
+    '''
+    # Se eliminan los saltos de línea y se separan los elementos por el espacio
+    lines = [x.strip("\n").split(" ") for x in lines]
     record_queue = ArrayQueue()
-    for i in range(len(lines)):
-        record_queue.record_queue.enqueue(Process(lines[i][0], lines[i][1], lines[i][2], lines[i][3], lines[i][4]))
+    # Se añaden los elementos a la cola de registros
+    for line in lines:
+        # id_proceso, id_usuario, tipo_recurso, tiempo_proceso, tiempo_ejecucion
+        record_queue.enqueue(Process(line[0], line[1], line[2], line[3], int(line[4])))
         
     return record_queue
             
-    
 def main():
-    # Penalizaciones a usuarios con tiempo de ejecucion alto y digan que es bajo
-    # Se aplica en el siguiente proceso del usuario que llegue a cualquiera de las 
-    # colas de ejecucion
-    
-    # Una unica cola de registros; se añaden todos los registros en orden de llegada y se
-    # procesan en orden de llegada a las colas correspondientes
+    '''
+    Función principal del programa.
+
+    Lee un archivo de entrada y realiza el procesamiento de los registros.
+    Realiza un seguimiento de los procesos y muestra las estadísticas finales.
+    '''
     with open(sys.argv[1]) as f:
+        # Leemos los datos y creamos el gestor de colas
         lines = f.readlines()
         record_queue = parse_file(lines)
         manager = ManagerQueue()
         
+        # Tiempo inicial
         time = 0
-        # Añadir mas condicones
-        while not(record_queue.is_empty()):
-            # Si tiene al menos un proceso en la cola se añade a la cola correspondientek
+        # Mientras la cola de registros/cola de procesos no esté vacía o haya procesos en ejecución
+        while not record_queue.is_empty() or not manager.all_empty() or not manager.all_processes_finished():
             time += 1
-            print(f"Time: {time}")
-            # Si la cola de registros no esta vacia se añaden a las colas correspondientes
-            if not(record_queue.is_empty()):
+            if not record_queue.is_empty():
+            # Si la cola de registros no esta vacia se añade el proceso a las cola correspondiente
                 actual_process = record_queue.dequeue()
+                # Actualizamos el tiempo de unión del proceso a la cola
                 actual_process._join_time = time
-                manager.manage_queue(actual_process)
-            manager.execute_process(record_queue, time)                
-            
+                # Lo insertamos en la cola correspondiente
+                manager.manage_queue(actual_process, time)
+            # Intentamos iniciar la ejeciciópn
+            manager.process_queues(time)
+            # Comprobamos si hay procesos que han terminado
+            manager.check_process(time)
 
-'''
-for i in range(len(manager.cpu_short)):
-    manager.cpu_short.dequeue().print()
-print(" ")
-for i in range(len(manager.cpu_long)):
-    manager.cpu_long.dequeue().print()
-print(" ")
-for i in range(len(manager.gpu_short)):
-    manager.gpu_short.dequeue().print()
-print(" ")
-for i in range(len(manager.gpu_long)):
-    manager.gpu_long.dequeue().print()
-print(" ")
-print(manager.print_all())
-'''    
+        # Generamos las estadísticas finales
+        penalization = Statistics().penalization_mean(manager.stats_penalties) # Penalizaciones por usuario
+        stay = Statistics().stay_mean(manager.stats_process)                   # Tiempo de espera por tipo de cola
+        print("\nPenalizaciones por usuario: \n-----------------------------\n", penalization)
+        print("\nTiempo de espera por tipo de cola: \n-----------------------------\n", stay)
+        # Mostramos las estadísticas finales en forma de grafico
+        Statistics().show_penalization_mean(penalization)
+        Statistics().show_stay_mean(stay)
+        
 if __name__ == "__main__":
     main()
